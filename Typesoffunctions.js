2c288f2fad98a9ffac5e0c3f7138ec08228fb9b0
NNkiran <kirannelikanti4545@gmail.com>
2024-12-23 18:42:50 +0530

    Added typesoffunctions.js File

---
+++ Typesoffunctions.js
@@ -0,0 +1,17 @@
+function greet(name){
+    return `Hello ${name}`
+}
+
+console.log(greet("Rohit"));
+
+let greet2 = function(name){
+    return `Hello ${name}`
+}
+
+console.log(greet2("RAM"));
+
+let greet3 = (name) =>{
+    return `Hello I am ${name}`
+}
+
+console.log(greet3("Surya"));
