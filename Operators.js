207c1dea08a39ba657839568355ca45c3b3de85a
NNkiran <kirannelikanti4545@gmail.com>
2024-12-23 18:23:13 +0530

    updated2 Operators.js File

--- Operators.js
+++ Operators.js
@@ -3,3 +3,11 @@ console.log(2**3);
 a = 10;
 console.log(++a);
 console.log(a++);
+
+
+//Checks only value not check the type
+console.log(5 == 5); //true
+console.log( 5 == '5'); // true
+
+console.log(5 === '5'); // false : here strict equals to checks the both value and type; false
+console.log(4 === 4); // true
